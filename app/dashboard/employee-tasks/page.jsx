acb3de4da0bf1026705177664aaf384b972b9d3e
rafa7lako <rafa7lako@gmail.com>
2024-10-17 22:38:48 +0200

    switched the dragging engine from draggable to framer motion. still not ready.

--- app/dashboard/employee-tasks/page.jsx
+++ app/dashboard/employee-tasks/page.jsx
@@ -1,28 +1,64 @@
+"use client";
 
-import TaskItem from "@/app/ui/dashboard/employee-tasks/taskItem";
+import { useState } from "react";
+import { Reorder } from "framer-motion";
 import classes from "./page.module.css";
 
 export default function Page() {
-    return (
-        <main>
-            <h1>Zadania pracowników</h1>
-            <div className={classes.itemContainer}>
-                {/* Time lines grid row */}
-                <div className={classes.timeLines}>
-                    <div className={classes.timeLineContainer}>
-                        <p className={classes.time}>7:00</p>
-                        <div className={classes.timeLine}></div>
-                    </div>
-                    <div className={classes.timeLineContainer}>
-                        <p className={classes.time}>8:00</p>
-                        <div className={classes.timeLine}></div>
-                    </div>
-                    {/* Add more timeLineContainer elements for each time */}
-                </div>
+	const [tasks, setTasks] = useState([1, 2, 3]);
 
-                {/* Draggable Task Items */}
-                <TaskItem />
-            </div>
-        </main>
-    );
+	return (
+		<main>
+			<h1>Zadania pracowników</h1>
+			<div className={classes.itemContainer}>
+				<div className={classes.timeLines}>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>7:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>8:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>9:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>10:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>11:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>12:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>13:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>14:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+					<div className={classes.timeLineContainer}>
+						<p className={classes.time}>15:00</p>
+						<div className={classes.timeLine}></div>
+					</div>
+				</div>
+				<Reorder.Group values={tasks} onReorder={setTasks}>
+					<div className={classes.employeeOneTasksRow}>
+						{tasks.map((task) => (
+							<Reorder.Item value={task} key={task}>
+								<div className={classes.taskItem}>{task}</div>
+							</Reorder.Item>
+						))}
+					</div>
+				</Reorder.Group>
+			</div>
+		</main>
+	);
 }
